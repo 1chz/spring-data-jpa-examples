69ba19ae06d58a4022d3adbf1485ec1f2d63a905
shirohoo <hch4102@naver.com>
2021-07-12 22:56:26 +0900

    feat: JPA 값타입 예제, 깊은복사 예제 추가
    Signed-off-by: shirohoo <hch4102@naver.com>

--- data-jpa/src/test/java/learn/jpa/model/domain/CustomerTest.java
+++ data-jpa/src/test/java/learn/jpa/model/domain/CustomerTest.java
@@ -20,7 +20,7 @@ class CustomerTest {
         Address address = Address.of("city", "street", "zipcode");
 
         Customer customer1 = Customer.of(name, "01000000001", address);
-        Customer customer2 = Customer.of(name, "01000000002", address.newInstance());
+        Customer customer2 = Customer.of(name, "01000000002", address.newInstance()); // 깊은복사
 
         Customer saveCustomer1 = repository.save(customer1); // insert 쿼리 작성(쓰기지연)
         Customer saveCustomer2 = repository.save(customer2); // insert 쿼리 작성(쓰기지연)
@@ -29,8 +29,8 @@ class CustomerTest {
         System.out.println("saveCustomer1 = " + saveCustomer1);
         System.out.println("saveCustomer2 = " + saveCustomer2);
 
-        customer1.changeAddress("changeCity changeStreet changeZipcode"); // update 쿼리 두번 작성(쓰기지연)
-        repository.flush(); // update 쿼리 두번 발생
+        customer1.changeAddress("changeCity changeStreet changeZipcode"); // update 쿼리 한번 작성(쓰기지연)
+        repository.flush(); // update 쿼리 한번 발생
 
         System.out.println("changeCustomer1 = " + saveCustomer1);
         System.out.println("changeCustomer2 = " + saveCustomer2);
