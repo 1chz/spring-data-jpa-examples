a580384c579c9bfc54cbdb0699ad6e28e3634a0d
shirohoo <hch4102@naver.com>
2021-07-24 02:26:30 +0900

    feat: JPQL 테스트코드 작성

--- src/test/java/learn/jpa/model/MemberTest.java
+++ src/test/java/learn/jpa/model/MemberTest.java
@@ -1,17 +1,14 @@
 package learn.jpa.model;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import learn.jpa.config.QuerydslConfig;
+import learn.jpa.config.JpaConfig;
 import learn.jpa.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 
 import java.util.List;
@@ -20,11 +17,15 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@RequiredArgsConstructor
-@Import(QuerydslConfig.class)
+@Import(JpaConfig.class)
 class MemberTest {
     private final JPAQueryFactory queryFactory;
     private final MemberRepository memberRepository;
+
+    public MemberTest(JPAQueryFactory queryFactory, MemberRepository memberRepository) {
+        this.queryFactory = queryFactory;
+        this.memberRepository = memberRepository;
+    }
 
     @ParameterizedTest
     @MethodSource("provideArgs")
